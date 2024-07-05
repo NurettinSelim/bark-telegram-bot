5eb97f319cbde1f53d8124d3d40af3065d9d9429
boraaoz <ozdekbora@gmail.com>
2024-07-05 15:51:38 +0300

    upt

--- main.py
+++ main.py
@@ -36,12 +36,11 @@ def hide_wallet_address(address: str) -> str:
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(
-        "Hi! My name is Bark. I am a bot that can help you with your Bonk account. "
-        "Please enter your public wallet key now."
+        "Hi! My name is Bark. Welcome to the bot that can help you with your Bonk account. "
+        "To use this application, please save your public key using the /save_public_key command."
     )
-    return 0
 
 async def save_public_key(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     await update.message.reply_text("Please enter your public wallet key now.")
@@ -63,7 +62,7 @@ async def public_key_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -
             ],
             [
                 InlineKeyboardButton("Balances", callback_data='balances'),
-                InlineKeyboardButton("PnL Graph", callback_data='pnl_graph'),
+                InlineKeyboardButton("PnL Details", callback_data='pnl_graph'),
             ],
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
@@ -72,7 +71,7 @@ async def public_key_input(update: Update, context: ContextTypes.DEFAULT_TYPE) -
         await update.message.reply_text(f"Error storing your public key: {e}")
     return ConversationHandler.END
 
-async def show_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def show_menu(update: Update, context: ContextTypes.DEFAULT_TYPE, text: str = "Choose an option:") -> None:
     keyboard = [
         [
             InlineKeyboardButton("Get Public Key", callback_data='get_public_key'),
@@ -84,12 +83,11 @@ async def show_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         ],
         [
             InlineKeyboardButton("Balances", callback_data='balances'),
-            InlineKeyboardButton("PnL Graph", callback_data='pnl_graph'),
+            InlineKeyboardButton("PnL Details", callback_data='pnl_graph'),
         ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    query = update.callback_query
-    await query.message.reply_text("Choose an option:", reply_markup=reply_markup)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text, reply_markup=reply_markup)
 
 async def get_public_key(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
@@ -114,31 +112,72 @@ async def remove_public_key(update: Update, context: ContextTypes.DEFAULT_TYPE) 
 async def get_total_volume(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
+    
+    user_public_key = mongo_client.bark.public_keys.find_one({"user_id": query.from_user.id})
+    if not user_public_key:
+        await query.edit_message_text("You have not saved your public key yet. Please save it with /save_public_key.")
+        return
+
+    total_volume_query = QueryBase(
+        name="Total Volume Query",
+        query_id=3777885,  # Replace with your actual query ID
+        params=[
+            QueryParameter(
+                name="Solana Wallet Address",
+                value=user_public_key["public_key"],
+                parameter_type=ParameterType.TEXT
+            ),
+        ]
+    )
+
     try:
-        total_volume = dune_client.get_latest_result(3777885).result.rows[0]["Volume"]
-        await query.edit_message_text(f"The total volume of Bonk is: {total_volume:.3f}")
+        fetching_message = await query.edit_message_text("Please wait while I fetch your total volume.")
+        
+        result = dune_client.run_query(query=total_volume_query, performance='medium')
+        total_volume = result.result.rows[0]["Volume"]
+        await fetching_message.edit_text(f"Your Wallet Total Volume is: ${total_volume:.3f}")
+        await show_menu(update, context)
     except Exception as e:
-        await query.edit_message_text(f"Error fetching total volume: {e}")
-    await show_menu(update, context)
+        await fetching_message.edit_text(f"Error fetching total volume: {e}")
 
 async def get_latest_volumes(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
     await query.answer()
+    
+    user_public_key = mongo_client.bark.public_keys.find_one({"user_id": query.from_user.id})
+    if not user_public_key:
+        await query.edit_message_text("You have not saved your public key yet. Please save it with /save_public_key.")
+        return
+
+    latest_volumes_query = QueryBase(
+        name="Latest Volumes Query",
+        query_id=3777907,  # Replace with your actual query ID
+        params=[
+            QueryParameter(
+                name="Solana Wallet Address",
+                value=user_public_key["public_key"],
+                parameter_type=ParameterType.TEXT
+            ),
+        ]
+    )
+
     try:
-        query_result = dune_client.get_latest_result(3777907)
-        sorted_result = sorted(query_result.result.rows, key=lambda x: x['Time'], reverse=True)
+        fetching_message = await query.edit_message_text("Please wait while I fetch the latest volumes.")
+
+        result = dune_client.run_query(query=latest_volumes_query, performance='medium')
+        sorted_result = sorted(result.result.rows, key=lambda x: x['Time'], reverse=True)
         latest_result_time = sorted_result[0]['Time']
-        latest_results = [result for result in sorted_result if result['Time'] == latest_result_time]
+        latest_results = [res for res in sorted_result if res['Time'] == latest_result_time]
         sorted_latest_results = sorted(latest_results, key=lambda x: x['Volume'], reverse=True)
 
-        message = f"Latest Volumes for Bonk ({latest_result_time}):"
-        for result in sorted_latest_results:
-            message += f"\n{result['token_bought_symbol']} : {result['Volume']:.3f}"
+        message = f"Latest Volumes ({latest_result_time}):"
+        for res in sorted_latest_results:
+            message += f"\n{res['token_bought_symbol']} : {res['Volume']:.3f}"
 
-        await query.edit_message_text(message)
+        await fetching_message.edit_text(message)
+        await show_menu(update, context)
     except Exception as e:
-        await query.edit_message_text(f"Error fetching latest volumes: {e}")
-    await show_menu(update, context)
+        await fetching_message.edit_text(f"Error fetching latest volumes: {e}")
 
 async def get_balances(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
@@ -150,31 +189,7 @@ async def get_balances(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     balances_query = QueryBase(
         name="Balances Query",
-        query_id=3808006,
-        params=[
-            QueryParameter(
-                name="Solana Wallet Address",
-                value=user_public_key["public_key"],
-                parameter_type=ParameterType.TEXT
-            ),
-        ]
-    )
-
-    total_portfolio_usd_query = QueryBase(
-        name="Total Portfolio USD Query",
-        query_id=3808045,
-        params=[
-            QueryParameter(
-                name="Solana Wallet Address",
-                value=user_public_key["public_key"],
-                parameter_type=ParameterType.TEXT
-            ),
-        ]
-    )
-
-    total_portfolio_sol_query = QueryBase(
-        name="Total Portfolio SOL Query",
-        query_id=3815789,
+        query_id=3852195,  # Updated query ID
         params=[
             QueryParameter(
                 name="Solana Wallet Address",
@@ -187,29 +202,33 @@ async def get_balances(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     try:
         fetching_message = await query.edit_message_text("Please wait while I fetch your balances.")
         
-        # Run all queries concurrently
+        # Run balances query
         balances_result = dune_client.run_query(query=balances_query, performance='medium')
-        total_usd_result = dune_client.run_query(query=total_portfolio_usd_query, performance='medium')
-        total_sol_result = dune_client.run_query(query=total_portfolio_sol_query, performance='medium')
+        balances_df = pd.DataFrame(balances_result.result.rows)
 
         # Debug: Print the results to check the returned structure
-        print("Balances Result:", balances_result.result.rows)
-        print("Total USD Result:", total_usd_result.result.rows)
-        print("Total SOL Result:", total_sol_result.result.rows)
+        print("Balances Result:", balances_df)
 
-        balances_data = balances_result.result.rows
+        balances_data = balances_df.to_dict(orient='records')
 
-        # Sort balances data by token value in USD
-        balances_data = sorted(balances_data, key=lambda x: float(x['token_value'] or 0), reverse=True)
+        # Sort balances data by token value in USD, handling NaN values
+        balances_data = sorted(balances_data, key=lambda x: float(x['token_usd_value'] or 0), reverse=True)
+
+        # Calculate total USD value
+        total_usd_value = sum(float(row['token_usd_value']) if pd.notna(row['token_usd_value']) else 0 for row in balances_data)
+
+        # Filter out tokens with less than 1% of the total value
+        token_values = [float(row['token_usd_value']) if pd.notna(row['token_usd_value']) else 0 for row in balances_data]
+        tokens = [row['token_symbol'][:6] for row in balances_data]  # truncate token symbols to 6 characters
+
+        token_percentages = [value / total_usd_value * 100 for value in token_values]
+        filtered_tokens = [(token, value) for token, value, percentage in zip(tokens, token_values, token_percentages) if percentage >= 1]
 
         # Create a pie chart
-        tokens = [row['token_symbol'][:6] for row in balances_data]  # truncate token symbols to 6 characters
-        token_values = [float(row['token_value']) for row in balances_data if row['token_value'] is not None]
-
         plt.figure(figsize=(10, 6))  # Increase the figure size
         wedges, texts, autotexts = plt.pie(
-            token_values,
-            labels=[f"{token} (${value:.2f})" for token, value in zip(tokens, token_values)],
+            [value for _, value in filtered_tokens],
+            labels=[f"{token} (${value:.2f})" for token, value in filtered_tokens],
             autopct='%1.1f%%',
             startangle=140
         )
@@ -232,35 +251,20 @@ async def get_balances(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
         # Prepare the message
         messages = f"Balances for your wallet address ({hide_wallet_address(user_public_key['public_key'])}):"
 
-        # Total portfolio value
-        try:
-            total_usd_value = total_usd_result.result.rows[0].get("Account_USD_Value", "N/A")
-        except (IndexError, KeyError) as e:
-            print(f"Error fetching total_usd_value: {e}")
-            total_usd_value = "N/A"
-
-        try:
-            total_sol_value = total_sol_result.result.rows[0].get("Account_SOL_Value", "N/A")
-        except (IndexError, KeyError) as e:
-            print(f"Error fetching total_sol_value: {e}")
-            total_sol_value = "N/A"
-
         messages += f"\n\n<b>Total Portfolio Value:</b>\n\n"
-        messages += f"USD: ${total_usd_value:.2f}" if total_usd_value != "N/A" else "USD: N/A"
-        messages += f"\nSOL: {total_sol_value:.3f}" if total_sol_value != "N/A" else "\nSOL: N/A"
+        messages += f"USD: ${total_usd_value:.2f}"
 
         messages += f"\n\n<b>Token Symbol</b> : Token Balance : <b>Total Token Value (USD)</b>"
         for row in balances_data:
-            if row['token_value']:
-                messages += f"\n<b>{row['token_symbol']}</b> : {float(row['token_balance']):.3f} : <b>${row['token_value']:.2f}</b>"
-            else:
-                messages += f"\n<b>{row['token_symbol']}</b> : {float(row['token_balance']):.3f} : N/A"
+            token_balance = float(row['token_balance']) if pd.notna(row['token_balance']) else 0
+            token_value = float(row['token_usd_value']) if pd.notna(row['token_usd_value']) else 0
+            messages += f"\n<b>{row['token_symbol']}</b> : {token_balance:.3f} : <b>${token_value:.2f}</b>"
 
         await fetching_message.delete()
         await context.bot.send_message(chat_id=query.message.chat_id, text=messages, parse_mode=ParseMode.HTML)
+        await show_menu(update, context)
     except Exception as e:
         await query.edit_message_text(f"Error fetching balances: {e}")
-    await show_menu(update, context)
 
 async def get_pnl_graph(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     query = update.callback_query
@@ -283,51 +287,51 @@ async def get_pnl_graph(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
     )
 
     try:
+        fetching_message = await query.edit_message_text("Please wait while I fetch your PnL data.")
+
         pnl_result = dune_client.run_query(query=pnl_query, performance='medium')
-        pnl_rows = pnl_result.result.rows
-        if not pnl_rows:
-            await query.edit_message_text("No PnL data available.")
+        pnl_df = pd.DataFrame(pnl_result.result.rows)
+        if pnl_df.empty:
+            await fetching_message.edit_text("No PnL data available.")
             return
+
+        pnl_df['total_pnl_usd'] = pnl_df['total_pnl_usd'].astype(float)
+        pnl_df = pnl_df.sort_values(by='total_pnl_usd', ascending=True)
+
+        plt.figure(figsize=(10, 6))
+        bars = plt.bar(pnl_df['token_symbol'], pnl_df['total_pnl_usd'], color=['red' if x < 0 else 'blue' for x in pnl_df['total_pnl_usd']])
+        plt.xlabel('Token')
+        plt.ylabel('PnL (USD)')
+        plt.title('PnL for each Token')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+
+        for bar in bars:
+            if bar.get_height() < 0:
+                bar.set_y(bar.get_height())
+
+        bio = BytesIO()
+        plt.savefig(bio, format='png')
+        bio.seek(0)
+        plt.close()
+
+        total_pnl_usd = pnl_df['total_pnl_usd'].sum()
+        pnl_status = "+" if total_pnl_usd > 0 else "-"
+
+        await context.bot.send_photo(chat_id=query.message.chat_id, photo=bio, caption=f"PnL for each Token\n\nTotal PnL (USD): {pnl_status}${abs(total_pnl_usd):.2f}")
+
+        pnl_message = "<b>Detailed PnL Data:</b>\n"
+        for _, row in pnl_df.iterrows():
+            pnl_message += f"<b>{row['token_symbol']}</b>: {row['total_pnl_usd']:.2f} $\n"
+
+        await fetching_message.delete()
+        await context.bot.send_message(chat_id=query.message.chat_id, text=pnl_message, parse_mode=ParseMode.HTML)
+        await show_menu(update, context)
     except Exception as e:
-        await query.edit_message_text(f"Error fetching PnL data: {e}")
-        return
-
-    df_pnl = pd.DataFrame(pnl_rows)
-    df_pnl['total_pnl_usd'] = df_pnl['total_pnl_usd'].astype(float)
-
-    df_pnl = df_pnl.sort_values(by='total_pnl_usd', ascending=True)
-
-    plt.figure(figsize=(10, 6))
-    bars = plt.bar(df_pnl['token_symbol'], df_pnl['total_pnl_usd'], color=['red' if x < 0 else 'blue' for x in df_pnl['total_pnl_usd']])
-    plt.xlabel('Token')
-    plt.ylabel('PnL (USD)')
-    plt.title('PnL for each Token')
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-
-    for bar in bars:
-        if bar.get_height() < 0:
-            bar.set_y(bar.get_height())
-
-    bio = BytesIO()
-    plt.savefig(bio, format='png')
-    bio.seek(0)
-    plt.close()
-
-    total_pnl_usd = df_pnl['total_pnl_usd'].sum()
-    pnl_status = "+" if total_pnl_usd > 0 else "-"
-
-    await context.bot.send_photo(chat_id=query.message.chat_id, photo=bio, caption=f"PnL for each Token\n\nTotal PnL (USD): {pnl_status}${abs(total_pnl_usd):.2f}")
-
-    pnl_message = "<b>Detailed PnL Data:</b>\n<hr>\n"
-    for _, row in df_pnl.iterrows():
-        pnl_message += f"<b>{row['token_symbol']}</b>: {row['total_pnl_usd']:.2f} $\n"
-
-    await context.bot.send_message(chat_id=query.message.chat_id, text=pnl_message, parse_mode=ParseMode.HTML)
-    await show_menu(update, context)
+        await fetching_message.edit_text(f"Error fetching PnL data: {e}")
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    await update.message.reply_text("Action cancelled.")
+    await update.message.reply_text("Please save valid public key. Save it with /save_public_key.")
     return ConversationHandler.END
 
 def main() -> None:
